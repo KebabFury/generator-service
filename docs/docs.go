8781a5fea1ed0c532955b0d3240e0b61770e5e64
Tom Poezd <poyarkov.work@yandex.ru>
2024-11-03 09:40:30 +0300

    fix swagger

--- docs/docs.go
+++ docs/docs.go
@@ -146,7 +146,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8000",
+	Host:             "generator-inno.poezd.xyz",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "API",
