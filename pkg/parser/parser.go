04885999056bd9763553f00f1b8e1e36bff6182a
Tom Poezd <poyarkov.work@yandex.ru>
2024-11-04 04:09:56 +0300

    upd

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -31,34 +31,34 @@ type ApiCall struct {
 	FormatedReturn string      `json:"formated_return,omitempty"`
 }
 
-func (c ApiCall) PathParameters() string {
+func (c ApiCall) PathParameters() template.HTML {
 	var s []string
 	for _, param := range c.Parameters {
 		if param.In == "path" {
 			s = append(s, fmt.Sprintf(`\"%s\": {%s}`, param.Name, param.Name))
 		}
 	}
-	return strings.Join(s, ", ")
+	return template.HTML(strings.Join(s, ", "))
 }
 
-func (c ApiCall) BodyParameters() string {
+func (c ApiCall) BodyParameters() template.HTML {
 	var s []string
 	for _, param := range c.Parameters {
 		if param.In == "body" {
 			s = append(s, fmt.Sprintf(`\"%s\": {%s}`, param.Name, param.Name))
 		}
 	}
-	return strings.Join(s, ", ")
+	return template.HTML(strings.Join(s, ", "))
 }
 
-func (c ApiCall) QueryParameters() string {
+func (c ApiCall) QueryParameters() template.HTML {
 	var s []string
 	for _, param := range c.Parameters {
 		if param.In == "query" {
 			s = append(s, fmt.Sprintf(`\"%s\": {%s}`, param.Name, param.Name))
 		}
 	}
-	return strings.Join(s, ", ")
+	return template.HTML(strings.Join(s, ", "))
 }
 
 type Parameter struct {
